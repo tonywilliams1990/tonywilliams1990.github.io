44fef0fd69c93f6dea2e806d70ad2a9f8639c5a5
tonywilliams1990 <tony.willimas.1990@gmail.com>
2020-12-23 11:53:41 +0000

    card_test

---
+++ cards/animate.js
@@ -0,0 +1,22 @@
+var i = 0;
+var greeting = 'Dear mom,';
+var title = 'Merry Christmas!';
+var subtitle = 'Looking forward to seeing you soon.';
+var signoff = 'Lots of love,';
+var sender = 'Tony \& Rachel';
+var speed = 50;
+
+var length = Math.max( greeting.length, title.length, subtitle.length, signoff.length, sender.length );
+
+function happyHolidays() {
+  if (i < length ){
+    document.getElementById("greeting").innerHTML += greeting.charAt(i);
+    document.getElementById("title").innerHTML += title.charAt(i);
+    document.getElementById("subtitle").innerHTML += subtitle.charAt(i);
+    document.getElementById("signoff").innerHTML += signoff.charAt(i);
+    document.getElementById("sender").innerHTML += sender.charAt(i);
+    i++;
+    
+    setTimeout(happyHolidays, speed);
+  }
+}
